566fb6b0ecff4f6949123f190c95f758fcf0d78e
ao <aolin@ymail.com>
2019-06-27 11:41:19 +0300

    started react-redux portion on indexjs only

--- clientside/src/index.js
+++ clientside/src/index.js
@@ -2,12 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import thunk from 'redux-thunk';
 
-import {createStore} from 'redux';
+
+import {createStore, applyMiddleware} from 'redux';
 import vacReducer from './state/vacReducer';
 import {Provider} from 'react-redux';
+import thunk from 'redux-thunk';
 
-var store = createStore(vacReducer);
+var store = createStore(vacReducer, applyMiddleware(thunk));
 
 ReactDOM.render(<Provider store={store}><App /></Provider>,
   document.getElementById('root')
